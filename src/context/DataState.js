1127f28fc91df926c2cbe2fd34a8f96e731f2955
leandro angeli <kurco91@hotmail.com>
2022-01-05 07:40:26 -0300

    get requests ok

--- src/context/DataState.js
+++ src/context/DataState.js
@@ -4,18 +4,34 @@ import axios from 'axios';
 export const DataProvider = createContext();
 export default function DataState(props) {
 	const [data, setData] = useState([]);
+	//SINGLE REQUEST
+	// useEffect(() => {
+	// 	const getData = () => {
+	// 		axios
+	// 			.get(process.env.REACT_APP_BACK_END_URI + '/users')
+	// 			.then((res) => {
+	// 				setData(res);
+	// 				console.log(res);
+	// 			})
+	// 			.catch((err) => console.log(err));
+	// 	};
+	// 	getData();
+	// }, []);
+	//SINGLE REQUEST
 
-	useEffect(() => {
-		const getData = () => {
-			axios
-				.get(process.env.REACT_APP_BACK_END_URI + '/users')
-				.then((res) => {
-					setData(res);
-				})
-				.catch((err) => console.log(err));
-		};
-		getData();
-	}, []);
+	// MULTIPLE REQUESTS
+
+	const getUsers = axios.get(`${process.env.REACT_APP_BACK_END_URI}/users`);
+	const getCategories = axios.get(
+		`${process.env.REACT_APP_BACK_END_URI}/products/categories`
+	);
+	const getProducts = axios.get(
+		`${process.env.REACT_APP_BACK_END_URI}/products/`
+	);
+	Promise.all([getUsers, getCategories, getProducts])
+		.then((data) => console.log(data))
+		.catch((err) => console.log(err));
+	// MULTIPLE REQUESTS
 
 	return (
 		<DataProvider.Provider value={data}>{props.children}</DataProvider.Provider>
